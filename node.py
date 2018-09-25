b5d643ba9fcd2a3ad0404d9a5d725f3675703ff5
Pål Larsen <paaledward@gmail.com>
2018-09-25 14:20:53 +0200

    renames textomap to utils, adds stage creation and print functions to utils.py, changes node.py for easier access to coordinates

---
+++ node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, nodeType, coordinates):
+        self.nodeType = nodeType
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+
+    def isPath():
+        self.nodeType = "O"
