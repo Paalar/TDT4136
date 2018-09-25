d4b25fd166f0f5397d3d14f4f60444bb80af4918
Pål Larsen <paaledward@gmail.com>
2018-09-25 19:43:20 +0200

    Creates the a* algorithm and returns a board with a chosen path

--- astar.py
+++ astar.py
@@ -1,41 +1,53 @@
+from utils import manhattanDistance, printStage
+import heapq
+
 class AStar:
     def __init__(self, start, goal, board):
-        this.start = start
-        this.goal = goal
-        this.board = board
-        this.start.gScore = 0
-        this.start.fScore = manhattanDistance(start,goal)
-        this.openSet = [start]
-        this.closedSet = []
-        this.cameFrom = {}
+        self.start = start
+        self.goal = goal
+        self.board = board
+        self.start.gScore = 0
+        #self.start.updateHeuristic(manhattanDistance(start,goal))
+        self.openSet = [start]
+        self.closedSet = [start]
+        self.cameFrom = {}
 
-    def reconstruct_path(cameFrom, current):
+    def reconstruct_path(self, cameFrom, current):
         total_path = []
-        while current in cameFrom.iterkeys():
+        while current in cameFrom.keys():
             current = cameFrom[current]
+            #if current != self.start:
+                #current.isPath()
             total_path.append(current)
         return total_path
 
-    def run():
-        while openSet:
-            current = heapq.heappop(openSet)
-            if current == goal:
-                return reconstruct_path(cameFrom, current)
+    def run(self):
+        while self.openSet:
+            current = heapq.heappop(self.openSet)
+            self.reconstruct_path(self.cameFrom, current)
+            if current == self.goal:
+                return self.reconstruct_path(self.cameFrom, current)
+            current.isPath()
+            printStage(self.board)
+            print("\n")
 
-            heapq.heappush(closedSet, current)
-
+            heapq.heappush(self.closedSet, current)
             for neighbor in current.neighbors:
-                if neighbor in closedSet:
+                if neighbor in self.closedSet:
+                    print("\n")
                     continue
 
                 tentative_gScore = current.gScore + 1 # All distances are 1 in this version of A*
+                print("Tentative score", tentative_gScore)
 
-                if neighbor not in openSet:
-                    heapq.heappush(openSet, neighbor)
+                if neighbor not in self.openSet:
+                    heapq.heappush(self.openSet, neighbor)
                 elif tentative_gScore >= neighbor.gScore:
                     continue
 
-                cameFrom[neighbor] = current
+                self.cameFrom[neighbor] = current
                 neighbor.gScore = tentative_gScore
-                neighbor.updateHeuristic(manhattanDistance(neighbor, goal))
-
+                neighbor.updateHeuristic(manhattanDistance(neighbor, self.goal))
+                print("Neighbor's fScore = ",neighbor.getFScore())
+                print("coordinates = ", neighbor.y, neighbor.x)
+            print("\n\n\n")
