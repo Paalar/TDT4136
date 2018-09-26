5d3466b7c4618cd744ee7377417b83e637796ff3
Johannes Kvamme <johanneskv96@gmail.com>
2018-09-26 11:01:40 +0200

    track path, visited and checked neighbors with lifo and full sort

--- astar.py
+++ astar.py
@@ -7,7 +7,6 @@ class AStar:
         self.goal = goal
         self.board = board
         self.start.gScore = 0
-        #self.start.updateHeuristic(manhattanDistance(start,goal))
         self.openSet = [start]
         self.closedSet = [start]
         self.cameFrom = {}
@@ -16,38 +15,38 @@ class AStar:
         total_path = []
         while current in cameFrom.keys():
             current = cameFrom[current]
-            #if current != self.start:
-                #current.isPath()
+            if current != self.start:
+                current.isPath()
             total_path.append(current)
         return total_path
 
     def run(self):
         while self.openSet:
-            current = heapq.heappop(self.openSet)
-            self.reconstruct_path(self.cameFrom, current)
+            current = self.openSet.pop()
             if current == self.goal:
                 return self.reconstruct_path(self.cameFrom, current)
-            current.isPath()
+            current.wasVisited()
+            print("----ITERATION----")
             printStage(self.board)
             print("\n")
 
-            heapq.heappush(self.closedSet, current)
+            self.closedSet.append(current)
             for neighbor in current.neighbors:
+                
                 if neighbor in self.closedSet:
-                    print("\n")
                     continue
 
                 tentative_gScore = current.gScore + 1 # All distances are 1 in this version of A*
-                print("Tentative score", tentative_gScore)
 
-                if neighbor not in self.openSet:
-                    heapq.heappush(self.openSet, neighbor)
-                elif tentative_gScore >= neighbor.gScore:
+                
+                if tentative_gScore >= neighbor.gScore:
                     continue
 
+                neighbor.wasChecked()
                 self.cameFrom[neighbor] = current
                 neighbor.gScore = tentative_gScore
                 neighbor.updateHeuristic(manhattanDistance(neighbor, self.goal))
-                print("Neighbor's fScore = ",neighbor.getFScore())
-                print("coordinates = ", neighbor.y, neighbor.x)
-            print("\n\n\n")
+                if neighbor not in self.openSet:
+                    self.openSet.append(neighbor)
+                    neighbor.initialQueuePlace = len(self.openSet)
+            self.openSet.sort(reverse=True, key=lambda a: (a.getFScore(), -1 * a.initialQueuePlace))
--- node.py
+++ node.py
@@ -8,9 +8,16 @@ class Node:
         self.gScore = math.inf
         self.heuristic = heuristic
         self.neighbors = []
+        self.initialQueuePlace = 0
 
     def isPath(self):
+        self.nodeType = "X"
+
+    def wasVisited(self):
         self.nodeType = "O"
+
+    def wasChecked(self):
+        self.nodeType = "q"
 
     def updateHeuristic(self, heuristic):
         self.heuristic = heuristic
@@ -22,4 +29,4 @@ class Node:
         self.neighbors.append(node)
 
     def __lt__(self, other):
-        return self.getFScore() < other.getFScore()
+        return self.getFScore() < other.getFScore() #   if self.getFScore() != other.getFScore() else self.queuePlace > other.queuePlace
